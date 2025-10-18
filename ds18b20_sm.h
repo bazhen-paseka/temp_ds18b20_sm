44a44c632c94bc2fff31b64a9eaea2b149cf358b
bazhen.paseka <bazhen.paseka@gmail.com>
2025-10-18 18:18:26 +0300

    temp work
    rename

--- ds18b20_sm.h
+++ ds18b20_sm.h
@@ -2,20 +2,36 @@
 #ifndef	TEMP_DS18B20_SM_H_INCLUDED
 #define	TEMP_DS18B20_SM_H_INCLUDED
 
-	#include "main.h"
 	#include <string.h>
 	#include <stdio.h>
-	#include "ds18b20_local_config.h"
-
+	#include "main.h"
+	#include "debug_gx.h"
+	#include "local_config.h"
 /***************************************************************/
 
-void DS18b20_ConvertTemp_SkipROM (void);
-int DS18b20_Get_Temp_SkipROM (void);
+	#define	READ_ROM			0x33
+	#define	MATCH_ROM			0x55
+	#define	SKIP_ROM			0xCC
+	#define	ALARM_SEARCH		0xEC
+	#define	SEARCH_ROM			0xF0
+	#define	CONVERT_TEMP		0x44
+	#define WRITE_SCRATCHPAD	0x4E
+	#define READ_SCRATCHPAD		0xBE
+	#define COPY_SCRATCHPAD		0x48
+	#define	RECALL_E2			0xB8
+	#define READ_POWER_SUPPLY	0xB4
 
-void DS18b20_ConvertTemp_MatchROM( char * _serial_numb);
-int DS18b20_Get_temp_MatchROM(char * _serial_numb);
+	#define BIT_IN_BYTE			8
+	#define SCRATCHPAD_SIZE		9
+	#define SERIAL_NUMB_SIZE	8
 
-void DS18b20_Print_serial_number(UART_HandleTypeDef * uart);
+/***************************************************************/
+	void	Ds18b20_Init_DWT_Delay			(void);
+	void 	Ds18b20_Print_serial_number		(void);
+	void	Ds18b20_ConvertTemp_SkipROM 	(void);
+	int 	Ds18b20_Get_Temp_SkipROM 		(void);
+	void 	Ds18b20_ConvertTemp_MatchROM	(char * _serial_numb);
+	int 	Ds18b20_Get_temp_MatchROM		(char * _serial_numb);
 
 /***************************************************************/
 /***************************************************************/
@@ -26,6 +42,6 @@ void DS18b20_Print_serial_number(UART_HandleTypeDef * uart);
 
 //		char serial_number[8] = {0x28, 0xFF, 0x55, 0x64, 0x4C, 0x04, 0x00, 0x20};
 //		char serial_number[8];
-//		DS18b20_Print_serial_number(&huart1);
+//		Ds18b20_Print_serial_number(&huart1);
 
 #endif	//	TEMP_DS18B20_SM_H_INCLUDED
