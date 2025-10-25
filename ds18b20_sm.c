26b9a6770a6d32bd3e1a9905010f4609bbfb8461
bazhen.paseka <bazhen.paseka@gmail.com>
2025-10-25 14:40:23 +0300

    activate CRC

--- ds18b20_sm.c
+++ ds18b20_sm.c
@@ -6,10 +6,13 @@
 
 	uint8_t Ds18b20_CRC8				(uint8_t* addr, uint8_t len);
 	void 	Read_Scratchpad_Match_ROM	(uint8_t* _scratchpad, uint8_t* _serial_numb);
+	int 	Read_Scratchpad_Match_ROM_CRC(uint8_t* _scratchpad, uint8_t* _serial_numb);
 	void 	Read_Scratchpad_Skip_ROM	(uint8_t* _scratchpad);
+	int 	Read_Scratchpad_Skip_ROM_CRC(uint8_t* _scratchpad);
 	void 	Send_serial					(uint8_t* _serial_numb);
 	void 	Read_serial					(uint8_t* _serial_numb);
 	void 	Read_Scratchpad				(uint8_t* _scratchpad);
+	int		Read_Scratchpad_CRC			(uint8_t* _scratchpad);
 	void 	Send_byte 					(uint8_t _byte);
 	void 	Send_bit					(uint8_t _bit);
 	uint8_t Read_byte					(void);
@@ -43,20 +46,37 @@ uint8_t Ds18b20_CRC8(uint8_t *addr, uint8_t len) {
 
 int Ds18b20_Get_temp_MatchROM (uint8_t* _serial_numb) {
 	uint8_t scratchpad[9];
-	Read_Scratchpad_Match_ROM(scratchpad, _serial_numb);
-	return (100 * ((scratchpad[1]<<8) | scratchpad[0]))/16;
+	int status_temp = Read_Scratchpad_Match_ROM_CRC(scratchpad, _serial_numb);
+	if (status_temp == 0 ) {
+		//return (100 * ((scratchpad[1]<<8) | scratchpad[0]))/16;
+		int temp_int = (scratchpad[1]<<8) | scratchpad[0];
+		if (CHECK_BIT(temp_int, 15)) {
+			temp_int = temp_int - 1 - 0xFFFF;
+		}
+		temp_int =  (temp_int * 100)/16;
+		return temp_int;
+	}
+	return -1;
 } /***************************************************************/
 
 int Ds18b20_Get_Temp_SkipROM (void) {
 	uint8_t scratchpad[9];
-	Read_Scratchpad_Skip_ROM(scratchpad);
-//	uint8_t crc1 = scratchpad[8];
-//	uint8_t crc2 = DS18B20_CRC8( (uint8_t*)scratchpad ,8);
-	int temp_int = (scratchpad[1]<<8) | scratchpad[0];
-	if (CHECK_BIT(temp_int, 15)) {
-		temp_int = temp_int - 1 - 0xFFFF;
+	int status_temp = Read_Scratchpad_Skip_ROM_CRC(scratchpad);
+	if (status_temp == 0 ) {
+		//return (100 * ((scratchpad[1]<<8) | scratchpad[0]))/16;
+		int temp_int = (scratchpad[1]<<8) | scratchpad[0];
+		if (CHECK_BIT(temp_int, 15)) {
+			temp_int = temp_int - 1 - 0xFFFF;
+		}
+		temp_int =  (temp_int * 100)/16;
+		return temp_int;
 	}
-	return (temp_int * 100)/16;
+	return -1;
+//	int temp_int = (scratchpad[1]<<8) | scratchpad[0];
+//	if (CHECK_BIT(temp_int, 15)) {
+//		temp_int = temp_int - 1 - 0xFFFF;
+//	}
+//	return (temp_int * 100)/16;
 } /***************************************************************/
 
 void Ds18b20_Print_serial_number (void) {
@@ -89,6 +109,19 @@ void Read_Scratchpad_Match_ROM (uint8_t* _scratchpad, uint8_t* _serial_numb){
 	}
 } /***************************************************************/
 
+int Read_Scratchpad_Match_ROM_CRC (uint8_t* _scratchpad, uint8_t* _serial_numb) {
+	memset(_scratchpad, 0xFF, SCRATCHPAD_SIZE);
+	uint8_t present = Start_strob();
+	if (present){
+		Send_byte(MATCH_ROM);		//	MATCH ROM (compare with serial number)
+		Send_serial(_serial_numb);
+		Send_byte(READ_SCRATCHPAD);	//	Read Scratchpad
+		uint8_t read_status = Read_Scratchpad_CRC(_scratchpad);
+		return read_status;
+	}
+	return -1;
+} /***************************************************************/
+
 void Read_Scratchpad_Skip_ROM (uint8_t* _scratchpad) {
 	memset(_scratchpad, 0xFF, SCRATCHPAD_SIZE);
 	uint8_t present = Start_strob();
@@ -97,8 +130,19 @@ void Read_Scratchpad_Skip_ROM (uint8_t* _scratchpad) {
 		Send_byte(READ_SCRATCHPAD);	//	Read Scratchpad
 		Read_Scratchpad(_scratchpad);
 	}
-}
-/***************************************************************/
+} /***************************************************************/
+
+int Read_Scratchpad_Skip_ROM_CRC (uint8_t* _scratchpad) {
+	memset(_scratchpad, 0xFF, SCRATCHPAD_SIZE);
+	uint8_t present = Start_strob();
+	if (present){
+		Send_byte(SKIP_ROM);		//	SKIP ROM (skip serial number)
+		Send_byte(READ_SCRATCHPAD);	//	Read Scratchpad
+		uint8_t read_status = Read_Scratchpad_CRC(_scratchpad);
+		return read_status;
+	}
+	return -1;
+} /***************************************************************/
 
 void Ds18b20_ConvertTemp_MatchROM (uint8_t* _serial_numb) {
 	uint8_t present = Start_strob();
@@ -107,8 +151,7 @@ void Ds18b20_ConvertTemp_MatchROM (uint8_t* _serial_numb) {
 		Send_serial(_serial_numb);
 		Send_byte(CONVERT_TEMP);	//	Read Scratchpad
 	}
-}
-/***************************************************************/
+} /***************************************************************/
 
 void Ds18b20_ConvertTemp_SkipROM (void) {
 	uint8_t present = Start_strob();
@@ -229,6 +272,18 @@ void Read_Scratchpad(uint8_t* _scratchpad) {
 	for (int i = 0; i < SCRATCHPAD_SIZE; i++) {
 		_scratchpad[i] = Read_byte();
 	}
+} /***************************************************************/
+
+int Read_Scratchpad_CRC(uint8_t* _scratchpad) {
+	for (int i = 0; i < SCRATCHPAD_SIZE; i++) {
+		_scratchpad[i] = Read_byte();
+	}
+	uint8_t crc1 = _scratchpad[8];
+	uint8_t crc2 = Ds18b20_CRC8( _scratchpad ,8);
+	if (crc1 == crc2) {
+		return 0;
+	}
+	return -1;
 } /***************************************************************/
 
 void Send_serial( uint8_t * _serial_numb) {
